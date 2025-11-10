edfdf05e0fbbbe8c9ec08653382cd55b8e757dd2
mcusimanana2795-source <mcusimanana2795@gmail.com>
2025-11-10 17:12:57 +0200

    App.jsx

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold">Hello, world!</h1>
+      <p>Welcome to your React + Tailwind site!</p>
+    </div>
+  );
+}
+
+export default App;
